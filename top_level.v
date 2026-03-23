b2c42792608acedfad912043f55ce1eb2c9617bb
Fallen_12 <86211094+Fallen-12@users.noreply.github.com>
2026-03-23 18:30:28 +1100

    idk whats here tbh

--- top_level.v
+++ top_level.v
@@ -8,14 +8,18 @@ module top_level (
 	// Leds
 	
 	// Switches
+	input CLOCK_50,
+	input [1:0] KEY,
+	output [9:0] LEDR,
+	input [9:0] SW
 	
 );
 
     nios_system u0 (
-        .clk_clk       (/* Connect the clock */),       
-        .reset_reset_n (/* Connect a Key for reset */), 
-        .ledr_export   (/* Connect all LEDs */),   
-        .sw_export     (/* Connect all Switches */)      
+        .clk_clk       (CLOCK_50),       
+        .reset_reset_n (KEY[0]), 
+        .ledr_export   (LEDR),   
+        .sw_export     (SW/* Connect all Switches */)      
     );
 
 	 
